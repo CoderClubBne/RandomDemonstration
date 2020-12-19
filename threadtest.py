bd4a4b13cfd876562472ce9cfe6ef68a29198245
Nick Curran <6274048+n-crrn@users.noreply.github.com>
2020-12-19 16:02:54 +1000

    Added requested thread test code.

---
+++ threadtest.py
@@ -0,0 +1,27 @@
+# Thread demonstration in python.
+# Author: Nicholas Curran 2020-12-19
+
+import queue
+import threading
+
+def thread_func(q):
+    print("Second thread started")
+    nextItem = q.get()
+    while nextItem != "quit":
+        print("Got next item: " + nextItem)
+        nextItem = q.get()
+    print("Exiting second thread")
+
+theQ = queue.Queue()
+# Main loop
+t = threading.Thread(target=thread_func, args=(theQ,))
+t.start()
+print("Main thread start")
+inputValue = ""
+while inputValue != "quit":
+    inputValue = input("Type an item: ")
+    theQ.put(inputValue)
+print("Main thread finished")
+t.join()
+
+print("Program finished")
